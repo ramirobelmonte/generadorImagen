71c89a71d23cf43f11469b1e2055dbb52faa7f3b
Ramiro Belmonte <ramirobelmon@gmail.com>
2022-01-28 15:05:56 -0300

    generador de imagenes automaticas

---
+++ script.js
@@ -0,0 +1,39 @@
+const selecImagen = document.querySelector('#selecImage');
+const precioImage = document.querySelector('#precioImage');
+const btnGenerar = document.querySelector('#btnGenerar');
+const canvas = document.querySelector('#postImage');
+
+let imagen;
+
+selecImagen.addEventListener('change',()=>{
+    const imageUrl = URL.createObjectURL(selecImagen.files[0]);
+
+    imagen = new Image();
+    imagen.src = imageUrl;
+
+    imagen.addEventListener('load',()=>{
+        actualizarImage(canvas, imagen, "");
+    })
+
+})
+
+btnGenerar.addEventListener('click',()=>{
+    actualizarImage(canvas, imagen, "$"+precioImage.value)
+})
+
+function actualizarImage(canvas, image, precio) {
+    const ctx = canvas.getContext("2d");
+    const width = image.width;
+    const height = image.height;
+
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(image, 0, 0);
+
+    ctx.font = '80px Poppins';
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'center'
+    ctx.textBaseLine = 'top'
+
+    ctx.fillText(precio, width/2, 915);
+}
