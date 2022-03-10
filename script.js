73a44ede344ba640752dcbdddaae334174d1d986
ramirobelmonte <ramirobelmon@gmail.com>
2022-03-10 20:29:24 -0300

    remodelacion completa

--- script.js
+++ script.js
@@ -1,88 +1,88 @@
-const selecImagen = document.querySelector('#selecImage');
-const precioImage = document.querySelector('#precioImage');
-const btnGenerar = document.querySelector('#btnGenerar');
-const btnCompartir = document.querySelector('#btnCompartir');
-const btnDescargar = document.querySelector('#btnDescargar');
-const btnprint = document.querySelector('#btnprint');
-const formulario = document.querySelector('#formulario');
-const nombre_producto = document.querySelector('#nombre_producto');
-const contenedor_producto = document.querySelector('#Cproducto');
-const canvas = document.querySelector('#postImage');
-
+let background = new Image()
+background.src = 'src/bg.png'
 let imagen;
 let posicionY;
-let producto = false;
 
 
+function generarImagen(canvas, background, corte, precio, producto) {
 
-function actualizarImage(canvas, image, precio, y, boolean) {
+    //rederizo el lienzo base
     const ctx = canvas.getContext("2d");
+    //renderizo el texto
     const ctx_producto = canvas.getContext("2d");
-    const width = image.width;
-    const height = image.height;
-    
+    const ctx_precio = canvas.getContext("2d");
+    const ctx_corte = canvas.getContext("2d");
+    //parametros de dimensiones
+    const width = background.width;
+    const height = background.height;
+    //asigno los parametros
     canvas.width = width;
     canvas.height = height;
-    
-    ctx.drawImage(image, 0, 0);
-    ctx_producto.drawImage(image, 0, 0);
-    
-    ctx.font = '80px Poppins';
-    ctx.fillStyle = 'black';
-    ctx.textAlign = 'center'
-    ctx.textBaseLine = 'middle'
-    
-    
-    ctx.fillText(precio, width/2, y);
-    
-    if (boolean) {
-        ctx_producto.font = '80px Poppins';
-        ctx_producto.fillStyle = 'orange';
-        ctx_producto.textAlign = 'center'
-        ctx_producto.textBaseLine = 'middle'
-        
-        ctx_producto.fillText(nombre_producto.value, width/2, y-140);
-    }
+    //renderizo
+    ctx.drawImage(background, 0, 0);
+    ctx.drawImage(corte, 850, 0);
+    //estilo del texto
+    ctx_precio.font = '100px Poppins';
+    ctx_precio.fillStyle = 'black';
+    ctx_precio.textAlign = 'left';
+    ctx_precio.textBaseLine = 'middle';
+    ctx_precio.fillText('$'+precio, width-730, height-150);
+
+    ctx_producto.font = '100px Poppins';
+    ctx_producto.fillStyle = 'orange';
+    ctx_producto.textAlign = 'center'
+    ctx_producto.textBaseLine = 'middle';        
+    ctx_producto.fillText(producto, width-550, height/4.5);
     
 }
 
 window.addEventListener('load', function () {
+    //reinicio el formulario
+    formulario.reset()
+    nameCarne.classList.add('hidden')
+    nameProduct.classList.add('hidden')
+    precio.classList.add('hidden')
     
-    selecImagen.addEventListener('change',()=>{
-        const imageUrl = selecImagen.value;
-        
-        if (selecImagen.value == 'src/Pantilla_A4.png') {
-            posicionY = 540+80;
-            producto = true;
-            contenedor_producto.classList.remove('hidden');
-            
-        }else{
-            posicionY = 915;
-            producto = false;
-            contenedor_producto.classList.add('hidden');
+    selectImage.addEventListener('change',(e)=>{   
+
+        nameCarne.classList.add('hidden')
+        nameProduct.classList.add('hidden')
+
+        let valor = e.target.selectedIndex
+
+        if(valor == 1){
+            nameCarne.classList.remove('hidden')
+            precio.classList.remove('hidden')
+
+            corteCarne.addEventListener('change',(e)=>{
+                let src = e.target.value
+                let corte = e.target.selectedOptions[0].text.toUpperCase()
+                imagen = new Image()
+                imagen.src = src
+
+                imagen.addEventListener('load',()=>{
+                    generarImagen(postImage, background, imagen, precioImage.value, corte)
+                    btnGenerar.classList.remove('disable')
+                })
+                
+                btnGenerar.addEventListener('click',()=>{
+                    generarImagen(postImage, background, imagen, precioImage.value, corte)
+
+                    btnDescargar.download = corte+'.jpeg'
+                    btnDescargar.href = postImage.toDataURL("image/jpeg",1);
+
+                    formulario.reset()
+                    btnDescargar.classList.remove('hidden')
+
+                })
+    
+            })
         }
-
-        imagen = new Image();
-        imagen.src = imageUrl;
-    
-        imagen.addEventListener('load',()=>{
-            actualizarImage(canvas, imagen, "", 0, producto);
-        })
-        btnGenerar.classList.remove('disable');
-        btnDescargar.classList.add('hidden');
+        else if(valor == 2){
+            console.log('chau')
+        }
+        else{
+            formulario.reset()
+        }
     })
-    
-    btnGenerar.addEventListener('click',()=>{
-        actualizarImage(canvas, imagen, "$"+precioImage.value, posicionY, producto)
-    
-        const dataURL = canvas.toDataURL("image/jpeg",1);
-        btnDescargar.href = dataURL;
-
-        formulario.reset();
-        btnDescargar.classList.remove('hidden');
-
-    })
-
 })
-
-
