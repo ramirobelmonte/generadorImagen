e71a3b942dc8096598ac4e33175387a055df4bf7
ramirobelmon <ramirobelmon@gmail.com>
2023-06-26 14:53:12 -0300

    formateo de css

--- script.js
+++ script.js
@@ -1,117 +1,103 @@
-let background = new Image()
-background.src = 'src/bg.png'
-let imagen;
-let posicionY;
+function renderizarImagen(canvas, bgUrl, productoUrl, productoE, nombre, nombreE, precio, precioE) {
+    return new Promise((resolve, reject) => {
 
+        const ctx = canvas.getContext('2d');
 
-function generarImagen(canvas, background, corte, precio, producto) {
+        // Cargamos el background
+        const background = new Image();
+        background.onload = () => {
+            //Dimensiones
+            canvas.width = background.width;
+            canvas.height = background.height;
+            // Renderizar la imagen en el canvas
+            ctx.drawImage(background, 0, 0);
 
-    //rederizo el lienzo base
-    const ctx = canvas.getContext("2d");
-    //renderizo el texto
-    const ctx_producto = canvas.getContext("2d");
-    const ctx_precio = canvas.getContext("2d");
-    const ctx_corte = canvas.getContext("2d");
-    //parametros de dimensiones
-    const width = background.width;
-    const height = background.height;
-    //asigno los parametros
-    canvas.width = width;
-    canvas.height = height;
-    //renderizo
-    ctx.drawImage(background, 0, 0);
-    //estilo del texto
-    ctx_precio.font = '100px Poppins';
-    ctx_precio.fillStyle = 'black';
-    ctx_precio.textAlign = 'left';
-    ctx_precio.textBaseLine = 'middle';
-    ctx_precio.fillText('$'+precio, width-730, height-150);
+            //Cargamos el producto
+            const producto = new Image();
+            producto.onload = () => {
+                ctx.shadowColor = "rgba(0,0,0,0.5)";
+                ctx.shadowBlur = 100;
+                ctx.shadowOffsetX = 0;
+                ctx.shadowOffsetY = 0
+                ctx.drawImage(producto, productoE[0], productoE[1]);
 
-    ctx_producto.font = '100px Poppins';
-    ctx_producto.fillStyle = 'black';
-    ctx_producto.textAlign = 'center'
-    ctx_producto.textBaseLine = 'middle';
-    ctx_producto.fillText(producto, width-550, height/4.5);
+                ctx.font = '90px Poppins';
+                ctx.fillStyle = 'black';
+                ctx.textAlign = 'center';
+                ctx.fillText(nombre, nombreE[0], nombreE[1]);
+                ctx.fillText(precio, precioE[0], precioE[1]);
 
-    ctx.shadowColor = "rgba(0,0,0,0.5)";
-    ctx.shadowBlur = 100;
-    ctx.shadowOffsetX = 0;
-    ctx.shadowOffsetY = 0
-    ctx.drawImage(corte, 850, 0);
+                // Resolver la promesa
+                resolve();
+            }
+            producto.onerror = () => {
+                // Rechazar la promesa si ocurre un error al cargar la imagen
+                reject(new Error('Error al cargar la imagen'));
+            };
 
+            producto.src = productoUrl;
+
+        };
+        background.onerror = () => {
+            // Rechazar la promesa si ocurre un error al cargar la imagen
+            reject(new Error('Error al cargar la imagen'));
+        };
+
+        background.src = bgUrl;
+    });
 }
 
+btnGenerar.addEventListener("click", () => {
+    // Obtener el canvas
+    const canvas = postImage;
+    // URL de la imagen a cargar
+    const bgUrl = selectImage.value;
+    const productoUrl = corteCarne.value;
+    const nombre = corteCarne.selectedOptions[0].text.toUpperCase();
+    const precio = "$" + precioImage.value;
+    let productoE = [0, 0];
+    let nombreE = [0, 0];
+    let precioE = [0, 0];
 
-function generarA4(canvas, background, precio, producto) {
+    if (selectImage.selectedIndex == 3) {
+        productoE = [0, 600];
+        nombreE = [540, 1500];
+        precioE = [540, 1625];
+    }
+    else if (selectImage.selectedIndex == 2) {
+        productoE = [-300, -200];
+        nombreE = [500, 700];
+        precioE = [500, 820];
+    }
+    else if (selectImage.selectedIndex == 1) {
+        productoE = [837, 0];
+        nombreE = [1330, 200];
+        precioE = [1330, 910];
+    }
 
-    //rederizo el lienzo base
-    const ctx = canvas.getContext("2d");
-    //parametros de dimensiones
-    const width = background.width;
-    const height = background.height;
-    //asigno los parametros
-    canvas.width = width;
-    canvas.height = height;
-    //renderizo
-    ctx.drawImage(background, 0, 0);
-    //estilo del texto
-    ctx_precio.font = '100px Poppins';
-    ctx_precio.fillStyle = 'black';
-    ctx_precio.textAlign = 'left';
-    ctx_precio.textBaseLine = 'middle';
-    ctx_precio.fillText('$'+precio, width-730, height-150);
-
-    ctx_producto.font = '100px Poppins';
-    ctx_producto.fillStyle = 'orange';
-    ctx_producto.textAlign = 'center'
-    ctx_producto.textBaseLine = 'middle';
-    ctx_producto.fillText(producto, width-550, height/4.5);
-
-}
-
-window.addEventListener('load', function () {
-    //reinicio el formulario adf
-    formulario.reset()
-
-    selectImage.addEventListener('change',(e)=>{
-
-        nameCarne.classList.add('hidden')
-        nameProduct.classList.add('hidden')
-
-        let valor = e.target.selectedIndex
-
-        if(valor == 1){
-            nameCarne.classList.remove('hidden')
-            precio.classList.remove('hidden')
-
-            corteCarne.addEventListener('change',(e)=>{
-                let src = e.target.value
-                let corte = e.target.selectedOptions[0].text.toUpperCase()
-                imagen = new Image()
-                imagen.src = src
-
-                imagen.addEventListener('load',()=>{
-                    generarImagen(postImage, background, imagen, precioImage.value, corte)
-                    btnGenerar.classList.remove('disable')
-                })
-
-                btnGenerar.addEventListener('click',()=>{
-                    generarImagen(postImage, background, imagen, precioImage.value, corte)
-
-                    btnDescargar.download = corte+'.jpeg'
-                    btnDescargar.href = postImage.toDataURL("image/jpeg",1);
-
-                    btnDescargar.classList.remove('hidden')
-
-                })
-
-            })
-        }
-        else if(valor == 2){
-            console.log('chau')
-        }
-        else{
-            formulario.reset()
-        }
-    })
+    // Llamar a la función para renderizar la imagen y esperar a que se complete
+    renderizarImagen(canvas, bgUrl, productoUrl, productoE, nombre, nombreE, precio, precioE)
+        .then(() => {
+            console.log('Imagen renderizada con éxito');
+            btnDescargar.removeAttribute('disabled');
+            // Aquí puedes realizar acciones adicionales después de que la imagen se haya renderizado correctamente
+        })
+        .catch((error) => {
+            console.error('Ocurrió un error al renderizar la imagen:', error);
+            // Aquí puedes manejar cualquier error que ocurra durante el proceso de renderizado
+        });
 })
+
+btnDescargar.addEventListener('click', () => {
+    // Exporta el lienzo a una imagen en base64
+    var dataURL = postImage.toDataURL({
+      format: 'jpeg',
+      quality: 0.8
+    });
+  
+    // Crea un enlace para descargar la imagen
+    var link = document.createElement('a');
+    link.href = dataURL;
+    link.download = 'canvas.jpg';
+    link.click();
+});
